1a049a03eaa6ac700ed3fb81b1101cc915aa9f02
naminguyen123 <nam30102004@gmail.com>
2026-03-05 22:49:05 +0700

    add home controller

---
+++ booking/src/main/java/com/taxi/booking/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.taxi.booking.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "Taxi Booking API is running";
+    }
+}
